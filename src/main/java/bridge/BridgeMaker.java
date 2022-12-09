a4ba51715ce592f920f16a8024d3cba060235860
the9kim <96895686+the9kim@users.noreply.github.com>
2022-12-09 10:15:08 +0900

    feat: 랜덤 번호 생성 및 문자열('U' 또는 'D') 변환

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,11 +1,15 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
+
+    private static final String UPPER_DIRECTION = "U";
+    private static final String LOWER_DIRECTION = "D";
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
@@ -18,6 +22,26 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        validate(size);
+        for (int i = 0; i < size; i++) {
+            int randomNumber = bridgeNumberGenerator.generate();
+            bridge.add(convertNumToDirection(randomNumber));
+        }
+        return bridge;
     }
+
+    private void validate(int size) {
+        if (size < 3 || size > 20) {
+            throw new IllegalArgumentException("[ERROR] 3이상 20이하 정수가 아닙니다.");
+        }
+    }
+
+    private String convertNumToDirection(int randomNumber) {
+        if (randomNumber == 1) {
+            return UPPER_DIRECTION;
+        }
+        return LOWER_DIRECTION;
+    }
+
 }
--- src/main/java/bridge/controller/InputController.java
+++ src/main/java/bridge/controller/InputController.java
@@ -1,5 +1,7 @@
 package bridge.controller;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
 import bridge.InputView;
 
 import java.util.List;
@@ -8,7 +10,8 @@ public class InputController {
     public List<String> createBridge() {
         while(true) {
             try {
-                InputView.readBridgeSize();
+                BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+                return bridgeMaker.makeBridge(InputView.readBridgeSize());
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
